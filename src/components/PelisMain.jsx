e162de247901adf62ce5dca4ec7f2e64f8246abd
JonathanRodriguezC <clashmc66@gmail.com>
2024-07-28 21:24:20 -0600

    move data

--- src/components/PelisMain.jsx
+++ src/components/PelisMain.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import DataPelis from '../app/pelis.json'
+import DataPelis from '../data/pelis.json'
 import { PelisPicker } from './PelisPicker'
 import { PelisList } from './PelisList'
 import Style from '../app/page.module.css'
